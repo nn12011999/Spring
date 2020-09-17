01d8f511ecd92f54f37792d240c29285ff4720b7
Nguyen <nn12011999@gmail.com>
2020-09-18 00:44:22 +0700

    add user entity, user repository

--- Spring/src/main/java/com/example/demo/Controller/WebController.java
+++ Spring/src/main/java/com/example/demo/Controller/WebController.java
@@ -1,18 +1,37 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.User;
+import com.example.demo.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/")
     public String index() {
+
         return "index";
     }
 
-    @GetMapping("/something")
-    public String something() {
-        return "something";
+    @PostMapping("/signin")
+    public String signin(@RequestBody User user)
+    {
+        userRepository.save(user);
+        return "success";
     }
+
+    @GetMapping("/signin")
+    public String sign() {
+
+        return "index";
+    }
+
 }
--- Spring/src/main/java/com/example/demo/Model/User.java
+++ Spring/src/main/java/com/example/demo/Model/User.java
@@ -6,43 +6,50 @@ import javax.persistence.*;
 @Table(name = "User")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
-    private String fistName;
+    private String firstName;
     private String lastName;
+    private String emailId;
+
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, String emailId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
 
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
 
-    public String getFistName() {
-        return fistName;
+    @Column(name = "first_name", nullable = false)
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public void setFistName(String fistName) {
-        this.fistName = fistName;
-    }
-
+    @Column(name = "last_name", nullable = false)
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public String getEmailName() {
-        return emailName;
+    @Column(name = "email_address", nullable = false)
+    public String getEmailId() {
+        return emailId;
     }
-
-    public void setEmailName(String emailName) {
-        this.emailName = emailName;
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
     }
-
-    private String emailName;
 }
